1a365e46ee7d13bbf5f8248d9e6623bdb7fe7713
Sean Hall <seanhall4217@gmail.com>
2021-10-26 22:46:30 -0400

    added simple coordinate finding script

---
+++ position.py
@@ -0,0 +1,8 @@
+# Use this to get the coordinates of anything.
+# 1) Put mouse where you want to get coordinates.
+# 2) Command-space, type "terminal," press enter, to open Terminal without moving mouse.
+# 3) Navigate to directory.
+# 4) Run 'python3 position.py'
+
+import pyautogui
+print(pyautogui.position())
